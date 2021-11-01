219d4008f85012fc45f3ac9d135fe73844d2cfc8
TheMajorN <nickbaranosky@gmail.com>
2021-11-01 02:41:17 -0400

    Fixed Slip Razor.  It no longer crashes the game :D

--- src/main/java/com/kingpins/magicitems/MagicItems.java
+++ src/main/java/com/kingpins/magicitems/MagicItems.java
@@ -2,13 +2,20 @@ package com.kingpins.magicitems;
 
 import com.kingpins.magicitems.core.init.*;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.client.renderer.entity.SpriteRenderer;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.function.Supplier;
 
 
 @Mod(MagicItems.MOD_ID)
@@ -20,6 +27,7 @@ public class MagicItems {
     public MagicItems() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
         ItemInit.ITEMS.register(bus);
         BlockInit.BLOCKS.register(bus);
@@ -28,6 +36,17 @@ public class MagicItems {
         TileEntityTypeInit.TILE_ENTITIES.register(bus);
 
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void registerEntityModels(Supplier<Minecraft> minecraft) {
+        ItemRenderer renderer = minecraft.get().getItemRenderer();
+
+        RenderingRegistry.registerEntityRenderingHandler(EntityTypeInit.SLIP_RAZOR_ENTITY.get(),
+                (rendererManager) -> new SpriteRenderer<>(rendererManager, renderer));
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        registerEntityModels(event.getMinecraftSupplier());
     }
 
     private void setup(final FMLCommonSetupEvent event)
