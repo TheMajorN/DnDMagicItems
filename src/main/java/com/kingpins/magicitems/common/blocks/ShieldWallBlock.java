436939928eaf8e91a7cf79fe1c6ff7fd4996a5d3
TheMajorN <nickbaranosky@gmail.com>
2021-12-23 03:52:10 -0500

    Shield of the Tower Semi-Working

--- src/main/java/com/kingpins/magicitems/common/blocks/ShieldWallBlock.java
+++ src/main/java/com/kingpins/magicitems/common/blocks/ShieldWallBlock.java
@@ -1,5 +1,6 @@
 package com.kingpins.magicitems.common.blocks;
 
+import com.kingpins.magicitems.core.init.BlockInit;
 import com.kingpins.magicitems.core.init.TileEntityTypeInit;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
@@ -7,8 +8,15 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 
@@ -19,16 +27,5 @@ public class ShieldWallBlock extends Block {
                         Material.WOOD, MaterialColor.COLOR_LIGHT_BLUE)
                 .sound(SoundType.WOOD));
 
-    }
-
-    @Override
-    public boolean hasTileEntity(BlockState state) {
-        return true;
-    }
-
-    @Nullable
-    @Override
-    public TileEntity createTileEntity(BlockState state, IBlockReader world) {
-        return TileEntityTypeInit.SHIELD_OTT_TILE_ENTITY_TYPE.get().create();
     }
 }
--- src/main/java/com/kingpins/magicitems/common/items/ShieldOfTheTowerItem.java
+++ src/main/java/com/kingpins/magicitems/common/items/ShieldOfTheTowerItem.java
@@ -1,10 +1,45 @@
 package com.kingpins.magicitems.common.items;
 
+import com.kingpins.magicitems.core.init.BlockInit;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
 import net.minecraft.item.ShieldItem;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
-public class ShieldOfTheTowerItem extends ShieldItem {
-    public ShieldOfTheTowerItem(Properties p_i48470_1_) {
-        super(p_i48470_1_);
+public class ShieldOfTheTowerItem extends Item {
+
+    public ShieldOfTheTowerItem(Properties properties) {
+        super(properties);
     }
 
+    @Override
+    public ActionResultType useOn(ItemUseContext useContext) {
+        PlayerEntity player = useContext.getPlayer();
+        World level = player.level;
+        BlockPos pos = useContext.getClickedPos();
+        BlockState state = level.getBlockState(pos);
+
+        if (!state.isAir()) {
+            level.setBlockAndUpdate(pos,
+                    BlockInit.SHIELD_OF_THE_TOWER_BLOCK.get().defaultBlockState());
+            for (int i = 0 ; i < 6 ; i++) {
+                level.setBlockAndUpdate(pos.east(i),
+                        BlockInit.SHIELD_OF_THE_TOWER_BLOCK.get().defaultBlockState());
+            }
+            for (int i = 0 ; i < 6 ; i++) {
+                level.setBlockAndUpdate(pos.west(i),
+                        BlockInit.SHIELD_OF_THE_TOWER_BLOCK.get().defaultBlockState());
+            }
+            for (int i = 0 ; i < 6 ; i++) {
+                level.setBlockAndUpdate(pos.above(i),
+                        BlockInit.SHIELD_OF_THE_TOWER_BLOCK.get().defaultBlockState());
+            }
+        }
+        return ActionResultType.SUCCESS;
+    }
 }
